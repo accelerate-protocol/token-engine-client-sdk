4f9709da01ad4d398280070c75c0b331e1cd03a0
ajaxsunrise <ajaxyu@163.com>
2025-09-28 15:24:44 +0800

    feat:adapter fund

--- example/vault_integration_test.go
+++ example/vault_integration_test.go
@@ -599,7 +599,7 @@ func createVault(t *testing.T, test *VaultLaunchIntegrationTest, fundingDuration
 	t.Logf("交易提交成功，TxHash: %s", submitResp.TxHash)
 
 	// 5. 等待 MQ 推送
-	mqMessageInterface := test.waitForMQMessage(t, submitResp.TxHash, client.MessageTypeRBFVaultLaunch)
+	mqMessageInterface := test.waitForMQMessage(t, submitResp.TxHash, client.MessageTypeVaultLaunch)
 	require.NotNil(t, mqMessageInterface)
 
 	// 类型断言
